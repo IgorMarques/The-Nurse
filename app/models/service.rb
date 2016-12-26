44df3660ea9772bfb0cc5a4b02f920f6e4c79922
Igor Marques <igormarquessilva@gmail.com>
2016-12-26 17:25:55 -0300

    Add url valitation and dependent destroy to Service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,8 +1,11 @@
 class Service < ApplicationRecord
-  has_many :statuses
-  has_many :outages
+  has_many :statuses, dependent: :destroy
+  has_many :outages, dependent: :destroy
 
   validates_presence_of :name, :url
 
+  validates :url, format: { with: /\A(http:\/\/[a-zA-Z]|https:\/\/)[a-zA-Z]/ }
+
   scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
 end
